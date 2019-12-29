d73c0d23f5f20f9bee7bb0a9b5aa08f35cd89b22
Alex Xie <xieweialex@gmail.com>
2019-12-29 14:31:13 -0800

    [LinkedList] Iternative solution for 2 Add two numbers

--- Math/AddTwoNumbers.swift
+++ Math/AddTwoNumbers.swift
@@ -15,19 +15,55 @@
  */
 
 class AddTwoNumbers {
-    func addTwoNumbers(l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        guard let l1 = l1 else {return l2}
-        guard let l2 = l2 else {return l1}
-        
-        let outputNode = ListNode((l1.val + l2.val)%10)
-        if l1.val + l2.val > 9 {
-            outputNode.next = addTwoNumbers(addTwoNumbers(l1.next, l2.next),
-                                            ListNode(1))
-        } else {
-            outputNode.next = addTwoNumbers(l1.next, l2.next)
-        }
-        
-        
-        return outputNode
+  func addTwoNumbers(l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    guard let l1 = l1 else {
+      return l2
     }
+    guard let l2 = l2 else {
+      return l1
+    }
+
+    let outputNode = ListNode((l1.val + l2.val)%10)
+    if l1.val + l2.val > 9 {
+      outputNode.next = addTwoNumbers(addTwoNumbers(l1.next, l2.next),
+                                      ListNode(1))
+    } else {
+      outputNode.next = addTwoNumbers(l1.next, l2.next)
+    }
+
+
+    return outputNode
+  }
+
+
+  func addTwoNumbersI(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+
+    let dummy = ListNode(0)
+    var curr: ListNode? = dummy 
+    var l1 = l1, l2 = l2
+    var carry = 0
+    while l1 != nil || l2 != nil {
+
+      let x = l1?.val ?? 0
+      let y = l2?.val ?? 0
+      let sum = x + y + carry 
+      carry = sum / 10 
+
+      curr?.next = ListNode(sum % 10)
+      curr = curr?.next
+      if l1 != nil {
+        l1 = l1?.next 
+      }
+      if l2 != nil {
+        l2 = l2?.next 
+      }
+    }
+
+    if carry > 0 { 
+      curr?.next = ListNode(1)
+    }
+
+    return dummy.next
+  }
+
 }
