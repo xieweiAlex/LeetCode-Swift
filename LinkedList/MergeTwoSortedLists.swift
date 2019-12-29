6c5e8eb206083c8f250e5e5b0cbaceb37b5f6f73
Alex Xie <xieweialex@gmail.com>
2019-12-29 14:12:35 -0800

    [LinkedList] Provide recursive solution for 21 merge two sorted list

--- LinkedList/MergeTwoSortedLists.swift
+++ LinkedList/MergeTwoSortedLists.swift
@@ -15,26 +15,52 @@
  */
 
 class MergeTwoSortedLists {
+
     func mergeTwoLists(l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        let dummy = ListNode(0)
-        var node = dummy
-        
-        var l1 = l1
-        var l2 = l2
-        
-        while l1 != nil && l2 != nil {
-            if l1!.val < l2!.val {
-                node.next = l1
-                l1 = l1!.next
-            } else {
-                node.next = l2
-                l2 = l2!.next
-            }
-            node = node.next!
+      let dummy = ListNode(0)
+      var curr = dummy
+
+      var l1 = l1, l2 = l2
+      while let tmpL1 = l1, let tmpL2 = l2 {
+        if tmpL1.val < tmpL2.val {
+          curr.next = tmpL1
+          l1 = tmpL1.next
+        } else {
+          curr.next = tmpL2
+          l2 = tmpL2.next
         }
-        
-        node.next = l1 ?? l2
-        
-        return dummy.next
+
+        guard let next = curr.next else {
+          return nil 
+        }
+        curr = next
+      }
+
+      curr.next = l1 ?? l2
+      return dummy.next
+    }
+
+    func mergeTwoListsI(l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+      if l1 == nil, l2 == nil {
+        return nil
+      }
+
+      if let l1 = l1, l2 == nil {
+
+        return l1 
+      } else if l1 == nil, let l2 = l2 {
+
+        return l2 
+      } else if var l1 = l1, var l2 = l2 {
+
+        if l1.val > l2.val {
+          (l1, l2) = (l2, l1)
+        }
+
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+      }       
+
+      return nil     
     }
 }
