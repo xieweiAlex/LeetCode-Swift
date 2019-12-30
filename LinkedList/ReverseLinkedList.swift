6d6c73a6c27a88349c4c6a1ee3ce8c29129cbb3a
Alex Xie <xieweialex@gmail.com>
2019-12-30 11:23:03 -0800

    [LinkedList] Add iterative solution for 206: Reverse Linked List

--- LinkedList/ReverseLinkedList.swift
+++ LinkedList/ReverseLinkedList.swift
@@ -14,33 +14,67 @@
  * }
  */
 
+public class ListNode {
+  public var val: Int
+  public var next: ListNode?
+  public init(_ val: Int) {
+    self.val = val
+    self.next = nil
+  }
+}
+
 class ReverseLinkedList {
-    func reverseList(head: ListNode?) -> ListNode? {
-        var temp: ListNode?
-        var first = head
-        
-        while first != nil {
-            let second = first!.next
-            
-            first!.next = temp
-            
-            temp = first
-            first = second
-        }
-        
-        return temp
+  func reverseList(_ head: ListNode?) -> ListNode? {
+    var pre: ListNode?
+    var head = head 
+    while let tempHead = head {
+      let node = tempHead.next 
+      tempHead.next = pre 
+      pre = tempHead
+      head = node 
     }
 
-    func reverseList(_ head: ListNode?) -> ListNode? {
-        guard let h = head, let next = h.next else {
-            return head
-        }
-  
-        let node = reverseList(next)
-  
-        next.next = h
-        h.next = nil
-  
-        return node
+    return pre
+  }
+
+  func reverseListI(_ head: ListNode?) -> ListNode? {
+    guard let h = head, let next = h.next else {
+      return head
     }
+
+    let node = reverseListI(next)
+
+    next.next = h
+    h.next = nil
+
+    return node
+  }
 }
+
+func printList(_ listNode: ListNode?) {
+
+  print("Print list begin: ----------")
+  var listNode = listNode
+  while let node = listNode {
+    print("node: ", node.val)
+    listNode = node.next
+  }
+}
+
+let node = ListNode(1)
+let first = ListNode(2)
+let second = ListNode(3)
+let third = ListNode(4)
+node.next = first
+first.next = second 
+second.next = third
+
+print("origin:  ---- ")
+printList(node)
+
+let solution = ReverseLinkedList()
+let reversedNode = solution.reverseList(node)
+
+printList(reversedNode)
+
+
