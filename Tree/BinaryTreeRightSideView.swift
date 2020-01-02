51b67f45b1f279d63cc3e236a5a7895cb728e06d
Alex Xie <xieweialex@gmail.com>
2020-01-02 14:47:52 -0800

    [Tree, DFS, BFS] Add DFS solution for 199 Binary Tree Right Side View

--- Tree/BinaryTreeRightSideView.swift
+++ Tree/BinaryTreeRightSideView.swift
@@ -17,33 +17,65 @@
  */
  
 class BinaryTreeRightSideView {
-    func rightSideView(_ root: TreeNode?) -> [Int] {
-        var res = [Int]()
-        var nodeQ = [TreeNode]()
-        
-        if let root = root {
-            nodeQ.append(root)
-        }
-        
-        while nodeQ.count > 0 {
-            var size = nodeQ.count
-            
-            for i in 0..<size {
-                let node = nodeQ.removeFirst()
-                
-                if i + 1 == size {
-                    res.append(node.val)
-                }
-                
-                if let left = node.left {
-                    nodeQ.append(left)
-                }
-                if let right = node.right {
-                    nodeQ.append(right)
-                }
-            }
-        }
-        
-        return res
+
+  // BFS 
+  func rightSideView(_ root: TreeNode?) -> [Int] {
+    guard let root = root else {
+      return []
     }
+
+    var res = [Int]()
+    var nodeQ = [root]
+
+    while nodeQ.count > 0 {
+      var size = nodeQ.count
+
+      for i in 0..<size {
+        let node = nodeQ.removeFirst()
+
+        if i == size - 1 {
+          res.append(node.val)
+        }
+
+        if let left = node.left {
+          nodeQ.append(left)
+        }
+        if let right = node.right {
+          nodeQ.append(right)
+        }
+      }
+    }
+
+    return res
+  }
+
+  // DFS
+  func rightSideViewI(_ root: TreeNode?) -> [Int] {
+    func getRightNode(_ root: TreeNode?, _ level: Int, _ dict: inout [Int: [Int]]) {
+      guard let root = root else {
+        return 
+      }
+      
+      // var list = dict[level, default: []]
+      // list.append(root.val)
+      // dict[level] = list
+      dict[level, default: []].append(root.val)
+      
+      getRightNode(root.left, level+1, &dict)
+      getRightNode(root.right, level+1, &dict)
+    }
+    
+    var dict = [Int: [Int]]() 
+    getRightNode(root, 0, &dict)
+
+    var res = [Int]()
+    for i in 0..<dict.keys.count {
+      if var list = dict[i], let val = list.popLast() {
+        res.append(val)
+      }
+    }
+   
+    return res
+  }
+
 }
